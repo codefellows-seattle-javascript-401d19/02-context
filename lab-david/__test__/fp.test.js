a09cfb7104676e601da0e274ab4b11ad96a01014
David Lindahl <davidalindahl@gmail.com>
2017-11-28 15:19:39 -0800

    fixed indention

--- lab-david/__test__/fp.test.js
+++ lab-david/__test__/fp.test.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const customFunctions = require('../lib/fp.js');
+
+describe('customFunctions.reduce', () => {
+  test('applies a function against an accumulator and each element in the array to reduce it to a single value', () => {
+    expect(customFunctions.reduce(
+      (accumulator, currentValue) => {
+        return accumulator + currentValue;
+      },
+      [3,3,3],
+      0
+    )).toBe(9);
+  });
+    
+  test('An exception should be thrown if the callback is not a function', () => {
+    expect(
+      () => {
+        customFunctions.reduce(`I'm totally a function ()`, [3,3,3], 0);
+      }
+    ).toThrow();
+  });
+});
--- lab-david/lib/fp.js
+++ lab-david/lib/fp.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const customFunctions = module.exports = {};
+
+customFunctions.reduce = (callback, collection, initialValue) => {
+  if(typeof callback !== 'function')
+    throw new TypeError('<callback> should be a function!');
+  return Array.prototype.reduce.call(collection, callback, intitalValue);
+};
+
