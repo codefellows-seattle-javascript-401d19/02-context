ef9bbeb48519103897061710994d32c3cae148e3
Robert Reed <robert.mc.reed@gmail.com>
2017-11-28 15:35:15 -0800

    first test set up, failing, time to make it pass

--- lab-rob/__test__/fp.test.js
+++ lab-rob/__test__/fp.test.js
@@ -2,4 +2,13 @@
 
 const fp = require('../lib/fp');
 
-describe();
+describe('fp.js tests', () => {
+  describe('When called correctly, fp.map should take in a callback function and an array/array-like object, and return an Array.', () => {
+    test('When called with an array, fp.map() should return a mapped array', () => {
+      expect(fp.map(
+        num => num * 2,
+        [1, 2, 3]
+      )).toBe([2, 4, 6]);
+    });
+  });
+});
