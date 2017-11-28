6cdfb766aa2e02b3f557bbf496bd079cec585d24
Catherine Looper <loopercat@gmail.com>
2017-11-28 14:28:02 -0800

    Ran npm init and jest install

--- lab-catherine/__test__/fp.test.js
+++ lab-catherine/__test__/fp.test.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+
+
+
+
+
+// Create a NodeJS module in the __test__/ named fp.test.js that asserts the correctness of the fp module.
+
+// Use BDD describe and test methods to define descriptive tests
+// Each test callback should aim to test a small well defined feature of a function
+// Write tests to ensure the fp module functions correctly error-check parameters
+// Assert that the correct errors are thrown with invalid arguments
+// Write tests to ensure the fp module functions returns the correct results when invoked with valid arguments
--- lab-catherine/lib/fp.js
+++ lab-catherine/lib/fp.js
@@ -1,1 +1,13 @@
 'use strict';
+
+
+
+
+
+// Create a NodeJS module in the lib/ directory named fp.js that exports an object. Create stand-alone map, filter, reduce, and slice functions using the call and apply function methods. Define each function using ES6 lexical arrow function syntax.
+
+// In each function error-check each parameter and throw an Error with a meaningful message if the function is invoked with invalid arguments. Do not use any third party libraries in the FP module.
+
+// fp.map and fp.filter should have the function signature (callback, collection) => Array
+// fp.reduce should have the function signature (callback, initialState, collection) => data
+// fp.slice should have the function signature (begin, end, collection) => Array
