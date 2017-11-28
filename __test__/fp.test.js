f7e3342b2fcd60f2c40a8214eb984378826d516c
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-28 15:33:07 -0800

    success tests for fp.filter

--- __test__/fp.test.js
+++ __test__/fp.test.js
@@ -90,5 +90,49 @@ describe('fp.js', () => {
         }).toThrow();
     });
   });
-
+  describe('fp.filter', () => {
+    // parameters are f(collection, callback)
+    test('return values should only have a length greater than 6', ()=>{
+      expect(customFunct.filter(
+        (password) => password.length > 6,
+        ['password','pass','1234']
+      ).toString()
+      ).toBe('password','pass','1234');
+    });
+    test('return values should be larger than 90', ()=>{
+      expect(customFunct.filter(
+        () => grades > 90,
+        [95, 97.5, 81, 99, 23]
+      ).toString()
+      ).toBe('95, 97.5, 81, 99, 23');
+    });
+    test('an exception should be thrown if error', ()=>{
+      expect(
+        () => {
+          customFunct.filter(
+            (x) => x+2,
+            ['1',2, null],
+            0);
+        }).toThrow();
+      expect(
+        () => {
+          customFunct.filter(
+            (x) => x+2,
+            1
+          );
+        }).toThrow();
+      expect(
+        () => {
+          customFunct.filter(
+            'lets do this',
+            [1, 2, 3]
+          );
+        }).toThrow();
+    });
+  });
 });
+
+
+// `fp.map` and `fp.filter` should have the function signature `(callback, collection) => Array`
+// * `fp.reduce` should have the function signature `(callback, initialState, collection) => data`
+// * `fp.slice` should have the function signature `(begin, end, collection) => Array`
