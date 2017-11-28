aff4bf32209f84fca8086a510ac0179bd25476cc
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-11-28 15:53:35 -0800

    finished the reduce code

--- lab-jacob/__test__/fp.test.js
+++ lab-jacob/__test__/fp.test.js
@@ -1,12 +1,40 @@
 'use strict';
 
-const fp  = require('../lin/fp');
+const fp  = require('../lib/fp');
+
+
+describe('fp.reduceGenerator', () => {
+  
+  test('should return value of aggregated values', () => {
+    expect(fp.reduceGenerator(1,2,3,4).toBe(10))
+  }); 
+});
 
 
 describe('fp.reduce', () => {
-  
   test('should aggregate all the values to the accumulator and return that accumulated value', () => {
+    expect(fp.reduce(
+      (accumulator, currentValue) => {
+        return accumulator + currentValue; 
+      },
+      [1,2,3],
+      0
+    )).toBe(6);
+  });
+  test('Exception Thrown If Callback Is Not A Function', () => {
+    expect(
+      () => {
+        fp.reduce('Not a Function', [1,2,3], 0);
+      }
+    ).toThrow();
+  }); 
+});
+
+
+describe('fp.map', () => {
+  test('', () => {
     
   });
   
 });
+
--- lab-jacob/lib/fp.js
+++ lab-jacob/lib/fp.js
@@ -1,3 +1,17 @@
 'use strict';
 
-const fp = module.exports = {}
+const fp = module.exports = {};
+
+fp.reduceGenerator = (...args) => {
+  return Array.prototype.reduce.call(args,
+    (accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+};
+
+fp.reduce = (callback, array, initialValue) => {
+  if (typeof callback !== 'function')
+    throw new TypeError('<Callback> Not A Function');
+
+  return Array.prototype.reduce.call(array, callback, initialValue);
+};
