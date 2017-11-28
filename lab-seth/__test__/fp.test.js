0cecdd6c2e9a6081410073af3ed9ef21cafdd8e6
SethDonohue <donohue.seth@gmail.com>
2017-11-28 15:34:44 -0800

    first test GREEN for fp.map

---
+++ lab-seth/__test__/fp.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fp = require('../lib/fp.js');
+
+describe('fp.test.js', () => {
+  describe('tests for fp.map', () => {
+    test('fp.map should return a new array pf elements with the callback function enacted on each of the  original array elements, if no errors are present', () => {
+      expect(fp.map(
+        (x => x * 2),
+        [1,2,3]
+      )).toEqual([2,4,6]);
+    });
+  });
+});
---
+++ lab-seth/lib/fp.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const fp = module.exports = {};
+
+fp.map = (callback, collection) => {
+  return Array.prototype.map.call(collection, callback);
+};
+
+fp.filter = (callback, collection) => {
+
+  return array;
+};
+
+fp.reduce = (callback, initialState, collection) => {
+
+  return data;
+};
+
+fp.slice = (begin, end, collection) => {
+
+  return array;
+};
