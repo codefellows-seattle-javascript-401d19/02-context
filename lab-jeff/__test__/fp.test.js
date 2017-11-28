9dfe4551f0b649971f7afa8b543c35a4f5d6cf5d
jjkusowski <jjkusowski@yahoo.com>
2017-11-28 14:24:47 -0800

    Files scaffolded.  First test written for map.

---
+++ lab-jeff/__test__/fp.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const fp = require('../lib/fp');
+
+describe('fp.js', () => {
+  describe('fp.map', () => {
+    test('map should return a new array which applies a function to each value of input array', () => {
+      expect(fp.map(x => x * 2, [1,2,3])).toEqual([2,4,6]);
+    });
+  });
+});
