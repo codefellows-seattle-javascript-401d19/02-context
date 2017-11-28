d307830c2552523954d0d082b0e2c638b5a4d3df
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-28 15:17:24 -0800

    set up second set of tests, beginning write of .map

--- __test__/fp.test.js
+++ __test__/fp.test.js
@@ -2,16 +2,6 @@
 
 const customFunct = require('../lib/fp.js');
 
-// P Module Tests
-//
-// Create a NodeJS module in the __test__/ named fp.test.js that asserts the correctness of the fp module.
-//
-// Use TDD describe and test methods to define descriptive tests
-// Each test callback should aim to test a small well defined feature of a function
-// Write tests to ensure the fp module functions correctly error-check parameters
-// Assert that the correct errors are thrown with invalid arguments
-// Write tests to ensure the fp module functions returns the correct results when invoked with valid arguments
-
 describe('fp.js', () => {
   describe('fp.reduce', () => {
     // parameters are f(collection, callback, initialValue)
@@ -22,6 +12,13 @@ describe('fp.js', () => {
         0)
       ).toBe(6);
     });
+    test('return value should be the product of the collection', ()=>{
+      expect(customFunct.reduce(
+        (x, y) => x*y,
+        [1,5,3],
+        1)
+      ).toBe(15);
+    });
     test('an exception should be thrown if error', ()=>{
       expect(
         () => {
@@ -30,14 +27,75 @@ describe('fp.js', () => {
             ['1',2, null],
             0);
         }).toThrow();
-      // expect(customFunct.addWithES6(1,2,3)).toBe(6);
+      expect(
+        () => {
+          customFunct.reduce(
+            (x, y) => x+y,
+            1,
+            0);
+        }).toThrow();
+      expect(
+        () => {
+          customFunct.reduce(
+            (x, y) => x+y,
+            [1,2,3],
+            'a');
+        }).toThrow();
+      expect(
+        () => {
+          customFunct.reduce(
+            'lets do this',
+            [1, 2, 3],
+            0);
+        }).toThrow();
     });
-
-    // test('return value should be the sum of all arguments', ()=>{
-    //   // parameter is f(x, y)
-    //   // arguments are f(1, 2)
-    //   expect(customFunct.addWithES6()).toBe(0);
-    // });
-
   });
+  describe('fp.map', () => {
+    // parameters are f(collection, callback)
+    test('return values should increment by two', ()=>{
+      expect(customFunct.map(
+        (x) => x+2,
+        [1,2,3]
+      )
+      ).toBe([3,4,5]);
+    });
+    test('return values should double', ()=>{
+      expect(customFunct.map(
+        (x) => x*2,
+        [1,2,3]
+      )
+      ).toBe([2,4,6]);
+    });
+    test('an exception should be thrown if error', ()=>{
+      expect(
+        () => {
+          customFunct.map(
+            (x) => x+2,
+            ['1',2, null],
+            0);
+        }).toThrow();
+      expect(
+        () => {
+          customFunct.map(
+            (x) => x+2,
+            1
+          );
+        }).toThrow();
+      expect(
+        () => {
+          customFunct.map(
+            (x) => x+'a',
+            [1,2,3]
+          );
+        }).toThrow();
+      expect(
+        () => {
+          customFunct.map(
+            'lets do this',
+            [1, 2, 3]
+          );
+        }).toThrow();
+    });
+  });
+
 });
