6544c6e6b06983e5641e80cf6062c1c8b2a66ea9
jjkusowski <jjkusowski@yahoo.com>
2017-11-28 15:27:25 -0800

    map function, tests, and exceptions complete.

--- lab-jeff/__test__/fp.test.js
+++ lab-jeff/__test__/fp.test.js
@@ -8,5 +8,21 @@ describe('fp.js', () => {
       expect(fp.map(x => x * 2, [1,2,3])).toEqual([2,4,6]);
       expect(fp.map(x => x, 'hello')).toEqual(['h', 'e', 'l', 'l', 'o']);
     });
+    test('map should return exception if first input is not a function', () => {
+      expect( () => {
+        fp.map('not a function', [1,2,3]);
+      }
+      ).toThrow();
+    });
+    test('map should return exception if 2nd input is not an array or string', () => {
+      expect( () => {
+        fp.map(x => x * 2, true);
+      }
+      ).toThrow();
+      expect( () => {
+        fp.map(x => x * 2, null);
+      }
+      ).toThrow();
+    });
   });
 });
--- lab-jeff/lib/fp.js
+++ lab-jeff/lib/fp.js
@@ -3,6 +3,10 @@
 const fp = module.exports = {};
 
 fp.map = (callback, collection) => {
+  if (typeof callback !== 'function')
+    throw new TypeError('<callback> should be a function()');
+  if (!collection.length)
+    throw new TypeError('<collection> should be a string or array');
   return Array.prototype.map.call(collection, callback);
 
 };
