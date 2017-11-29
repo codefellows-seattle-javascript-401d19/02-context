69aee0e1aabcd0a1265e69dadd24720044eea858
Robert Reed <robert.mc.reed@gmail.com>
2017-11-28 18:10:42 -0800

    added test for map to check that it is working with ES5 arguments object and ES6 args object. Passed

--- lab-rob/__test__/fp.test.js
+++ lab-rob/__test__/fp.test.js
@@ -3,7 +3,7 @@
 const fp = require('../lib/fp');
 
 describe('fp.js tests', () => {
-  describe('When called correctly, fp.map should take in a callback function and an array/array-like object, and return an Array.', () => {
+  describe('When called correctly, fp.map should take in a callback function and an array / array-like object, and return an Array.', () => {
     test('When called with an array, fp.map() should return a mapped array', () => {
       expect(fp.map(
         num => num * 2,
@@ -14,6 +14,26 @@ describe('fp.js tests', () => {
         word => word + word,
         ['hi', 'there']
       )).toEqual(['hihi', 'therethere']);
+    });
+
+    test('When called with arguments are args, an acted-on array should be returned.', () => {
+      expect(
+        (function(){
+          return fp.map(
+            num => num * 2,
+            arguments
+          );
+        })(1, 1.5, 5)
+      ).toEqual([2, 3, 10]);
+
+      expect(
+        ((...args) => {
+          return fp.map(
+            word => word.toUpperCase(),
+            args
+          );
+        })('i', 'use', 'sPrEaD!')
+      ).toEqual(['I', 'USE', 'SPREAD!']);
     });
 
     test('When called with a non-array, but iterable object, like a string, the object should be returned as an array with its elements acted upon.', () => {
