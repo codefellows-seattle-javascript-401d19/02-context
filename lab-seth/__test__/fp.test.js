0176dd81c2d080df5daf57b10050c29e8445f05d
SethDonohue <donohue.seth@gmail.com>
2017-11-28 16:09:31 -0800

    TypeError test GREEN for fp.map

--- lab-seth/__test__/fp.test.js
+++ lab-seth/__test__/fp.test.js
@@ -4,11 +4,21 @@ const fp = require('../lib/fp.js');
 
 describe('fp.test.js', () => {
   describe('tests for fp.map', () => {
+
     test('fp.map should return a new array pf elements with the callback function enacted on each of the  original array elements, if no errors are present', () => {
       expect(fp.map(
         (x => x * 2),
         [1,2,3]
       )).toEqual([2,4,6]);
+    });
+
+    test('An exception should be thrown if callback is not a function', () => {
+      expect(
+        () => {
+          fp.map(
+            'not a function',
+            [1,2,3]);
+        }).toThrow();
     });
   });
 });
--- lab-seth/lib/fp.js
+++ lab-seth/lib/fp.js
@@ -3,6 +3,10 @@
 const fp = module.exports = {};
 
 fp.map = (callback, collection) => {
+
+  if(typeof callback !== 'function')
+    throw new TypeError('<callback> should be a function');
+
   return Array.prototype.map.call(collection, callback);
 };
 
