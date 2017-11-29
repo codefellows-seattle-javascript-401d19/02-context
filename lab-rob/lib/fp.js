64c378270a239c1dd6da26bc01109b1e0e7e8a87
Robert Reed <robert.mc.reed@gmail.com>
2017-11-28 17:18:21 -0800

    refactored map to check explicitly for undefined, rather than falsy

--- lab-rob/lib/fp.js
+++ lab-rob/lib/fp.js
@@ -3,7 +3,7 @@
 const fp = module.exports = {};
 
 fp.map = (callback, collection) => {
-  if(!collection) throw new TypeError('fp.map(callback, collection) requires two arguments');
+  if(collection === undefined) throw new TypeError('fp.map(callback, collection) requires two arguments');
   if(collection.length === undefined) throw new TypeError('<collection> must be an iterable object.');
   return [].map.call(collection, callback);
 };
