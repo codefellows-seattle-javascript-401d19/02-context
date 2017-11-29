7eae4a20a857760023bfe3135f32c49c501086af
Predrag Josifovic <pjosifovic@yahoo.com>
2017-11-28 18:18:39 -0800

    adding fp.reduce tests and function

--- lab-pedja/__test__/fp.test.js
+++ lab-pedja/__test__/fp.test.js
@@ -4,32 +4,86 @@ const fp = require('../lib/fp');
 
 describe('fp.js', () => {
 
+  // testing fp.map function
   describe('fp.map', () => {
+
     test('returns array with values multiplied with 2', () => {
       expect(fp.map(
         (num) => num * 2,
         [0, 1, 2]
       )).toEqual([0, 2, 4]);
     });
-    test('exception will be thrown if callback is not a function', () => {
+
+    test('exception will be thrown if error occurs', () => {
       expect(
         () => {
           fp.map('I\'m not a function ()', [0, 1, 2]);
-        }).toThrow();
+      }).toThrow();
+      expect(
+        () => {
+          fp.map(
+            (num) => num * 2,
+            'one'
+          );
+      }).toThrow();
     });
   });
 
-  // describe('fp.filter', () => {
-  //   test('')
-  // });
-  //
-  // describe('fp.reduce', () => {
-  //   test('')
-  // });
-  //
-  // describe('fp.slice', () => {
-  //   test('')
-  // });
+  // testing fp.filter function
+  describe('fp.filter', () => {
 
+    test('return value should be number higher than 3', () => {
+      expect(fp.filter(
+        (num) => num > 3,
+        [1, 2, 3, 4, 5]
+      )).toEqual([4, 5]);
+    });
+
+    test('exception will be thrown if error occurs', () => {
+      expect(
+        () => {
+          fp.filter('I\'m not a function ()', [0, 1, 2]);
+      }).toThrow();
+      expect(
+        () => {
+          fp.filter(
+            (num) => num > 3,
+            'one'
+          );
+      }).toThrow();
+    });
+  });
+
+  // testing fp.reduce function
+  describe('fp.reduce', () => {
+
+    test('return value should be sum of the collection if callback is adding them', () => {
+      expect(fp.reduce(
+        (accu, curr) => {
+          return accu + curr;
+        },
+        [1, 2, 3],
+        10
+      )).toBe(16);
+    });
+
+    test('exception will be thrown if error occurs', () => {
+      expect(
+        () => {
+          fp.reduce('I\'m not a function ()', [0, 1, 2], 10);
+        }
+      ).toThrow();
+      expect(
+        () => {
+          fp.reduce(
+            (accu, curr) => {
+              return accu + curr;
+            },
+            [0, 1, 2],
+            'someString');
+        }
+      ).toThrow();
+    });
+  });
 
 });
--- lab-pedja/lib/fp.js
+++ lab-pedja/lib/fp.js
@@ -5,5 +5,23 @@ const fp = module.exports = {};
 fp.map = (callback, collection) => {
   if(typeof callback !== 'function')
     throw new TypeError('<callback> should be a function');
+  if(typeof collection !== 'object')
+    throw new TypeError('<collection> should be an array like object');
   return Array.prototype.map.call(collection, callback);
+};
+
+fp.filter = (callback, collection) => {
+  if(typeof callback !== 'function')
+    throw new TypeError('<callback> should be a function');
+  if(typeof collection !== 'object')
+    throw new TypeError('<collection> should be an array like object');
+  return Array.prototype.filter.call(collection, callback);
+};
+
+fp.reduce = (callback, collection, initialValue) => {
+  if(typeof callback !== 'function')
+    throw new TypeError('<callback> is not a function');
+  if(typeof initialValue !== 'number')
+    throw new TypeError('<initialValue> is not a number')
+  return Array.prototype.reduce.call(collection, callback, initialValue);
 };
