17d591f2e1e47216de3b7cf54b03f5133e2bb389
Robert Reed <robert.mc.reed@gmail.com>
2017-11-28 16:26:22 -0800

    map test passing with string input

--- lab-rob/__test__/fp.test.js
+++ lab-rob/__test__/fp.test.js
@@ -15,5 +15,18 @@ describe('fp.js tests', () => {
         ['hi', 'there']
       )).toEqual(['hihi', 'therethere']);
     });
+
+    test('When called with a non-array, but iterable object, the object should be returned as acted upon.', () => {
+      expect(fp.map(
+        char => char + char,
+        'hey'
+      )).toEqual(['hh', 'ee', 'yy']);
+    });
+
+    test('If the first argument is not an function, an exception should be thrown.', () => {
+      expect(() => {
+        fp.map('not a function', [1, 2, 3]);
+      }).toThrow();
+    });
   });
 });
--- lab-rob/lib/fp.js
+++ lab-rob/lib/fp.js
@@ -2,4 +2,7 @@
 
 const fp = module.exports = {};
 
-fp.map = (callback, collection) => [].map.call(collection, callback);
+fp.map = (callback, collection) => {
+  // if(typeof callback !== 'function') throw TypeError;
+  return [].map.call(collection, callback);
+};
