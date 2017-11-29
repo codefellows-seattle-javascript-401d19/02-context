90fd3d63b48aab9ed11aee3e0a4ce1ddf5b4cdcc
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2017-11-29 04:07:50 -0800

    added filter and slice to fp js file

--- lab02-fredric/lib/fp.js
+++ lab02-fredric/lib/fp.js
@@ -20,3 +20,25 @@ fp.reduce = (callback, collection, initialValue) => {
 
   return Array.prototype.reduce.call(collection, callback, initialValue);
 };
+
+fp.filter = (callback, collection) => {
+
+  if (typeof callback !== 'function')
+    throw new TypeError('<callback> should be a function');
+
+  if (!Array.isArray(collection))
+    throw new TypeError('<collection> should be an array');
+
+  return Array.prototype.filter.call(collection, callback);
+};
+
+fp.slice = (begin, end, collection) => {
+
+  if (!Array.isArray(collection))
+    throw new TypeError('<collection> should be an array');
+
+  if (typeof begin !== 'number' || typeof end !== 'number')
+    throw new TypeError('<begin> and <end> should be a number');
+
+  return Array.prototype.slice.call(collection, begin, end);
+};
