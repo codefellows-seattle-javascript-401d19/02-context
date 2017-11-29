2475200683523c32f7cf5e7cd28cf2a7f94e5686
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-11-28 16:32:06 -0800

    deleted unneeded function

--- lab-jacob/__test__/fp.test.js
+++ lab-jacob/__test__/fp.test.js
@@ -2,15 +2,6 @@
 
 const fp  = require('../lib/fp');
 
-
-describe('fp.reduceGenerator', () => {
-  
-  test('should return value of aggregated values', () => {
-    expect(fp.reduceGenerator(1,2,3,4).toBe(10))
-  }); 
-});
-
-
 describe('fp.reduce', () => {
   test('should aggregate all the values to the accumulator and return that accumulated value', () => {
     expect(fp.reduce(
@@ -21,7 +12,7 @@ describe('fp.reduce', () => {
       0
     )).toBe(6);
   });
-  test('Exception Thrown If Callback Is Not A Function', () => {
+  test('In Reduce Exception Thrown If Callback Is Not A Function', () => {
     expect(
       () => {
         fp.reduce('Not a Function', [1,2,3], 0);
@@ -31,10 +22,16 @@ describe('fp.reduce', () => {
 });
 
 
+describe('fp.mapGenerator', () => {
+  test('should return value of aggregated values', () => {
+    expect(fp.mapGenerator(1,2,3,4).toBe(2,3,4,5));
+  }); 
+});
+
 describe('fp.map', () => {
-  test('', () => {
+  
+  test('should change each element in the array and return a mutated array', () => {
     
   });
   
-});
-
+})
--- lab-jacob/lib/fp.js
+++ lab-jacob/lib/fp.js
@@ -3,15 +3,11 @@
 const fp = module.exports = {};
 
 fp.reduceGenerator = (...args) => {
+  if (typeof callback !== 'function')
+    throw new TypeError('<Callback> Not A Function');
+
   return Array.prototype.reduce.call(args,
     (accumulator, currentValue) => {
       return accumulator + currentValue;
     }, 0);
-};
-
-fp.reduce = (callback, array, initialValue) => {
-  if (typeof callback !== 'function')
-    throw new TypeError('<Callback> Not A Function');
-
-  return Array.prototype.reduce.call(array, callback, initialValue);
 };
