a1f8e12c79eb295f528be00a450ec47580650575
Andrew Bloom <blooma116@gmail.com>
2017-11-28 16:58:49 -0800

    3 out of 4 functions set up for testing

--- lab-andrew/lib/fp.js
+++ lab-andrew/lib/fp.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const fp = module.exports = {};
+
+fp.map = (callback, collection) => {
+  if (typeof callback !== 'function') throw new TypeError('callback must be function');
+  if (typeof collection !== 'object') throw new TypeError('collection must be an array');
+  if (collection.length < 1) throw new TypeError('collection must contain values');
+  collection.forEach(e => {
+    if (typeof e !== 'number') throw new TypeError('all array values must be numbers');
+  });
+  return Array.prototype.map.call(collection, callback);
+};
+
+fp.filter = (callback, collection) => {
+  if (typeof callback !== 'function') throw new TypeError('callback must be function');
+  if (typeof collection !== 'object') throw new TypeError('collection must be an array');
+  if (collection.length < 1) throw new TypeError('collection must contain values');
+  collection.forEach(e => {
+    if (typeof e !== 'number') throw new TypeError('all array values must be numbers');
+  });
+  return Array.prototype.filter.call(collection, callback);
+};
+
+fp.reduce = (callback, collection, initialValue) => {
+  if (typeof callback !== 'function') throw new TypeError('callback must be function');
+  if (typeof collection !== 'object') throw new TypeError('collection must be an array');
+  if (collection.length < 1) throw new TypeError('collection must contain values');
+  collection.forEach(e => {
+    if (typeof e !== 'number') throw new TypeError('all array values must be numbers');
+  });
+  return Array.prototype.reduce.call(collection, callback, initialValue);
+};
